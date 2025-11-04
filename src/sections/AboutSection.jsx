ad7768683cf2db4ef73dbf97803e3c26cd9b586e
krativeweb <krativewebdeveloper@gmail.com>
2025-11-04 18:26:04 +0530

    Update AboutSection.jsx

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -14,7 +14,7 @@ export default function AboutSection() {
             >
               <h6 className="subtitle text-warning">About Us</h6>
               <h2 className="title">
-                Shapingss Future Leaders Through Holistic Education
+                Shaping Future Leaders Through Holistic Education
               </h2>
               <p style={{ textAlign: "justify" }}>
                 Institute of Management Technology, Hyderabad (IMT Hyderabad) is
