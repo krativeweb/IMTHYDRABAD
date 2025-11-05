13ac41b2313dc9583308a421e6188135b7e47d23
krativeweb <krativewebdeveloper@gmail.com>
2025-11-05 18:04:37 +0530

    Update page.jsx

--- src/app/scholarship-and-professionalism/page.jsx
+++ src/app/scholarship-and-professionalism/page.jsx
@@ -208,7 +208,7 @@ export default function StandardsOfScholarship() {
         </div>
 
         {/* Policy Content */}
-        <section id="executive-education" className="py-5">
+         <section id="executive-education" className="py-5">
           <div className="container" data-aos="fade-up" data-aos-delay="200">
             <div className="card p-4">
               <h2
