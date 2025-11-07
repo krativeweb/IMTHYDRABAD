abaf0a2c8ee84afa61dd94a29e54914e5f9d646f
krativeweb <krativewebdeveloper@gmail.com>
2025-11-07 18:29:08 +0530

    update

--- src/components/NavbarTwo.jsx/page.jsx
+++ src/components/NavbarTwo.jsx/page.jsx
@@ -90,7 +90,7 @@ export default function NavbarTwo() {
       </div>
 
       {/* Navbar */}
-     <nav   ref={navbarRef}  className="navbar navbar-expand-lg navbar-light sticky-top mt-4">
+     <nav   ref={navbarRef}  className="navbar navbar-expand-lg navbar-light sticky-top" style={{ marginTop:'39px' }}>
     <div className="container shadow-sm">
       {/* Logo */}
       <a className="navbar-brand" href="/">
