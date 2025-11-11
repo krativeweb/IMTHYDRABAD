0f206234be840ea972446f2559f007ddbdb5cd78
krativeweb <krativewebdeveloper@gmail.com>
2025-11-11 17:41:13 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="robots" content="noindex, nofollow" />
+<meta name="robots" content="index, follow" />
+
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
