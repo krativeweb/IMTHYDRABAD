1553e3412318312408bc94b4051f06d2953d1244
krativeweb <krativewebdeveloper@gmail.com>
2025-11-22 22:28:19 +0530

    Update ClarityScript.jsx

--- src/components/Analytics/ClarityScript.jsx
+++ src/components/Analytics/ClarityScript.jsx
@@ -1,23 +1,19 @@
-// components/Analytics/ClarityScript.jsx
-"use client";
-
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function ClarityScript() {
-  useEffect(() => {
-  
-
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.innerHTML = `
-      (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "tnv5etuna8");
-    `;
-    document.head.appendChild(script);
-  }, []);
-
-  return null;
+  return (
+    <Script
+      id="clarity-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "tnv5etuna8");
+        `,
+      }}
+    />
+  );
 }
