267270def4e91f9444d857bb17237f74789ac7b2
krativeweb <krativewebdeveloper@gmail.com>
2025-11-28 15:46:25 +0530

    Update page.jsx

--- src/app/faculties/page.jsx
+++ src/app/faculties/page.jsx
@@ -4,7 +4,7 @@ import FacultiesClient from "@/seo-page/FacultiesClient";
 // 🟢 Apply faculty page SEO for ALL faculty profiles
 export async function generateMetadata() {
   return getMetadata("/faculties/");
-}
+}  
 
 export default function Faculty() {
   return <FacultiesClient />;
