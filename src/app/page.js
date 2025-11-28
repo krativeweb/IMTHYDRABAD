9723f97aeef41c2f28be619efc4f0c5452931e02
krativeweb <krativewebdeveloper@gmail.com>
2025-11-28 10:48:50 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,47 +1,10 @@
-// app/page.jsx
-'use client'
-import AnnouncementStrip from "@/components/AnnouncementStrip";
-import VideoSection from "@/sections/VideoSection";
-import AboutSection from "@/sections/AboutSection";
-import USPSection from "@/sections/USPSection";
-import ResearchCarousel from "@/sections/ResearchCarousel";
-import ProgramsSection from "@/sections/ProgramsSection";
-import HappeningsSection from "@/sections/HappeningsSection";
-import EventsSection from "@/sections/EventsSection";
-import AnnouncementsSection from "@/sections/AnnouncementsSection";
-import ConnectSection from "@/sections/ConnectSection";
-import InternationalAssociations from "@/sections/InternationalAssociations";
-import CampusLifeGallery from "@/sections/CampusLifeGallery";
-import PlacementAlliances from "@/sections/PlacementAlliances";
+import { getMetadata } from "@/utils/getMetadata";
+import HomeClient from "@/seo-page/Home";
 
-export default function Home() {
+export async function generateMetadata() {
+  return getMetadata("/");
+}
 
-
-  return (
-    <>
-      <style jsx global>{`
-        .navbar {
-          margin-top: 41px !important;
-        }
-      `}</style>
-      {/* Announcement Strip */}
-      <AnnouncementStrip />
-
-      {/* Add body class for padding */}
-
-      {/* All Page Sections */}
-      <VideoSection />
-      <AboutSection />
-      <USPSection />
-      <ResearchCarousel />
-      <ProgramsSection />
-      <HappeningsSection />
-      <EventsSection />
-      <AnnouncementsSection />
-      <ConnectSection />
-      <InternationalAssociations />
-      <CampusLifeGallery />
-      <PlacementAlliances />
-    </>
-  );
+export default function Page() {
+  return <HomeClient />;
 }
