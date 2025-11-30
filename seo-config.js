bea9e35f4c80b4f8fe9a385014322cbf76b326d8
krativeweb <krativewebdeveloper@gmail.com>
2025-11-30 12:23:42 +0530

    update

--- seo-config.js
+++ seo-config.js
@@ -15,8 +15,7 @@ const SEO = {
     canonical: "https://www.imthyderabad.edu.in/faculty",
   },
 
-
-   "/faculties": {
+  "/faculties": {
     title: "Faculty of management studies | IMT Hyderabad",
     description:
       "Explore Faculty of Management Studies at IMT Hyderabad known for academic excellence, innovative learning, global exposure, and strong placement opportunities.",
@@ -24,14 +23,13 @@ const SEO = {
     canonical: "https://www.imthyderabad.edu.in/faculty",
   },
 
-    "/research-publication": {
+  "/research-publication": {
     title: "Research publications | IMT Hyderabad",
     description:
       "Discover research publications at IMT Hyderabad showcasing innovative studies, impactful insights, and academic excellence across management and allied fields.",
     keywords: "Research publications",
     canonical: "https://www.imthyderabad.edu.in/research-publication",
   },
-
 
   "/research-archive": {
     title: "Research Archives | IMT Hyderabad",
@@ -40,54 +38,78 @@ const SEO = {
     keywords: "Research Archives",
     canonical: "https://www.imthyderabad.edu.in/research-archive",
   },
-    "/centre-csr": {
+  "/centre-csr": {
     title: "Centre for Sustainability and CSR | IMT Hyderabad",
     description:
       "Explore IMT Hyderabad’s Centre for Sustainability and CSR, fostering research, innovation, and impactful initiatives for responsible management and society.",
     keywords: "Sustainability and CSR",
     canonical: "https://www.imthyderabad.edu.in/centre-csr",
   },
-    "/centre-cdt": {
+  "/centre-cdt": {
     title: "Centre for Digital Transformation | IMT Hyderabad",
     description:
       "Explore IMT Hyderabad’s Centre for Digital Transformation, driving research, innovation, and learning in technology, AI, analytics, and digital business.",
     keywords: "Digital Transformation",
     canonical: "https://www.imthyderabad.edu.in/centre-cdt",
   },
-    "/innovation-lab": {
+  "/innovation-lab": {
     title: "Innovation Lab | IMT Hyderabad",
     description:
       "IMT Hyderabad Innovation Lab fosters creativity, research, and entrepreneurship through cutting-edge projects, collaboration, and impactful innovations.",
     keywords: "Innovation Lab",
     canonical: "https://www.imthyderabad.edu.in/innovation-lab",
   },
-    "/awards-and-recognisation": {
+  "/awards-and-recognisation": {
     title: "Awards and Recognitions | IMT Hyderabad",
     description:
       "IMT Hyderabad Awards and Recognitions highlight faculty and student achievements, academic excellence, industry impact, and contributions to management education.",
     keywords: "Awards and Recognitions",
     canonical: "https://www.imthyderabad.edu.in/awards-and-recognisation",
   },
-    "/about-imt-hyderabad": {
+  "/about-imt-hyderabad": {
     title: "Top PGDM Colleges in Hyderabad | IMT Hyderabad ",
     description:
       "Explore the top PGDM colleges in Hyderabad that offer excellent faculty, industry-ready curriculum and high placement rates. Find your ideal management institute today.",
     keywords: "Top pgdm colleges in hyderabad",
     canonical: "https://www.imthyderabad.edu.in/about-imt-hyderabad",
   },
-    "/admissions/pgdm": {
+  "/admissions/pgdm": {
     title: "Admission | IMT Hyderabad | PGDM Colleges in India",
     description:
       "Apply for admission at IMT Hyderabad, one of the top PGDM colleges in India, offering industry-driven management programs with strong placements and alumni network.",
     keywords: "pgdm colleges in india",
     canonical: "https://www.imthyderabad.edu.in/admissions/pgdm",
   },
-    "/fpm-admission": {
+  "/fpm-admission": {
     title: "Fellow Prgram In Management | IMT Colleges | IMT Hyderabad",
     description:
       "Explore top IMT Colleges offering world-class management programs, industry exposure, and a corporate learning environment for future business leaders.",
     keywords: "IMT Colleges",
     canonical: "https://www.imthyderabad.edu.in/fpm-admission",
+  },
+
+  "/pgdm-course-in-hyderabad": {
+    title: "pgdm program | IMT Hyderabad",
+    description:
+      "Advance your career with our PGDM program, gain industry-ready skills, expert mentorship, and global exposure for a successful management future..",
+    keywords: "pgdm program",
+    canonical: "https://www.imthyderabad.edu.in/pgdm-course-in-hyderabad",
+  },
+
+  "/pgdm-general-management": {
+    title: "PGDM | IMT Hyderabad | Best PGDM colleges in India",
+    description:
+      "Explore IMT Hyderabad, one of the best PGDM colleges in India, offering a career-focused PGDM course, excellent faculty, and placement opportunities",
+    keywords: "Best PGDM colleges in India, PGDM course",
+    canonical: "https://www.imthyderabad.edu.in/pgdm-general-management",
+  },
+
+  "/pgdm-finance-management": {
+    title: "PGDM Finance | IMT Hyderabad | PG Diploma In Finance",
+    description:
+      "Explore PGDM Finance at IMT Hyderabad, one of the top B-schools in India, offering a comprehensive PG Diploma in finance with strong industry exposure.",
+    keywords: " PGDM Finance , PG Diploma in finance",
+    canonical: "https://www.imthyderabad.edu.in/pgdm-finance-management",
   },
 };
 
