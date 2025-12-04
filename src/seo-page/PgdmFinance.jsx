60b40182e4c8a09ef5150c967660671728c887b6
krativeweb <krativewebdeveloper@gmail.com>
2025-12-04 22:55:19 +0530

    update

--- src/seo-page/PgdmFinance.jsx
+++ src/seo-page/PgdmFinance.jsx
@@ -54,7 +54,7 @@ export default function PGDMFinanceSection() {
           __html: `
       /* Faculty Hero */
       .faculty-hero {
-        background: url("/media/banners/pgdmfinance.webp") center/cover no-repeat;
+        background: url("/media/banners/pgdmmarketing.webp") center/cover no-repeat;
         position: relative;
         height: 60vh;
       }
