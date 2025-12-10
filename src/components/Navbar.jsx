eb4d3708c2322965ff79481e0698bfbd70854715
krativeweb <krativewebdeveloper@gmail.com>
2025-12-10 14:52:55 +0530

    Adjust Navbar logo margin for better alignment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,7 +73,7 @@ export default function Navbar() {
               alt="Logo"
               width={270} // 2x retina
               height={100}
-              style={{ margin: "2px" }}
+              style={{ margin: "2px",margin-left:"17px" }}
             />
           </Link>
 
