97d291d85418a0e1e583cbb0af0b175b94eee2b2
krativeweb <krativewebdeveloper@gmail.com>
2026-01-06 15:31:39 +0530

    Update FooterTwo.jsx

--- src/components/FooterTwo.jsx
+++ src/components/FooterTwo.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
           <div className="row">
             <div className="col-sm-12">
               <div className="text-center pb-15 mt-3">
-                Copyright © 2025
+                Copyright © 2026
                 <span className="text-light fw-bold"> IMT HYDERABAD.</span> All
                 Rights Reserved.
               </div>
