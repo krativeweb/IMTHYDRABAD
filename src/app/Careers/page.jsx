e6c4821e385368dae4836a59c35659ea239cfd36
krativeweb <krativewebdeveloper@gmail.com>
2026-02-04 11:34:33 +0530

    Update page.jsx

--- src/app/Careers/page.jsx
+++ src/app/Careers/page.jsx
@@ -1,7 +1,7 @@
 import { getMetadata } from "@/utils/getMetadata";
 import CareersClient from "@/seo-page/Carrers";
 
-// 🟢 Apply faculty page SEO for ALL faculty profiles
+// 🟢 Apply faculty page SEO for ALL faculty profiles 
 export async function generateMetadata() {
   return getMetadata("/careers", "/api/mandatory/slug/careers");
 }
