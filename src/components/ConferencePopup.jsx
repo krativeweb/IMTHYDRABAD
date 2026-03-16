4c1ca5acb537971bb70013b1e0192de5fa0f688c
krativeweb <krativewebdeveloper@gmail.com>
2026-03-16 21:53:37 +0530

    UPDATE

--- src/components/ConferencePopup.jsx
+++ src/components/ConferencePopup.jsx
@@ -1,39 +1,42 @@
 "use client";
 
 export default function ConferenceBanner() {
-  return ( 
+  return (
     <div className="conference-banner">
-      <a href="https://isdsiglobal.com/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://isdsiglobal.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img
-          src="/imt-global.png"
+          src="/neww.png"
           alt="ISDSI Global Conference"
           className="banner-img"
+         
         />
       </a>
 
-      <style jsx>{`
-        .conference-banner {
-          width: 100%;
-          background: #d6f0e0;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+ <style jsx>{`
+  .conference-banner {
+    width: 100%;
+    position: absolute;
+    top: 158px;
+    z-index: 1000; /* bring banner above video */
+  }
 
-        .banner-img {
-          width: 100%;
-          max-width: 1400px;
-          height: auto;
-          display: block;
-          cursor: pointer;
-        }
-
+  .banner-img {
+    width: 100%;
+    height: auto;
+    display: block;
+    cursor: pointer;
+  }
+     /* Mobile adjustment */
         @media (max-width: 768px) {
-          .banner-img {
-            width: 100%;
+          .conference-banner {
+            top: 170px; /* increase space for mobile navbar */
           }
         }
-      `}</style>
+`}</style>
     </div>
   );
 }
